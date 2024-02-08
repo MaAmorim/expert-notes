4d2a409561665bfc77899d1a19a2bf4ca9c5b548
Matheus Amorim <matheus.amorim3110@hotmail.com>
2024-02-08 19:32:32 -0300

    fix: fix te position of modals

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -23,7 +23,7 @@ export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50">
-          <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-tranzinc-x-1/2 md:-tranzinc-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-zinc-700 md:rounded-md flex flex-col outline-none">
+          <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-zinc-700 md:rounded-md flex flex-col outline-none">
             <Dialog.Close className="absolute right-0 top-0 bg-zinc-800 p-1.5 text-zinc-400 hover:text-zinc-100">
               <X className="size-5" />
             </Dialog.Close>
